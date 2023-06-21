8bd2b189c65cca2f17efbee2e28829bfd77d467a
Little-F1sh <itsnihon1@gmail.com>
2023-06-21 22:11:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+// class -14.--variable.
+
+var address = "dinajpur";
+// console.log(address);
+
+var myNameis = "shahariwar";
+// console.log(myNameis, address);
+
+var price = 20;
+// console.log(price, address);
+
+var doYouEatRice = true ;
+// console.log(doYouEatRice);
+
+var doYouEatRice = false ;
+// console.log(doYouEatRice);
+
+var sakib = 300;
+var tamim =200;
+
+var total = sakib + tamim;
+// console.log(total);
+
+var Defference = sakib - tamim;
+// console.log(Defference);
+
+var Division = sakib / tamim;
+// console.log(Division);
+
+var Division = sakib % tamim;
+// console.log(Division);
+
+var multiplication = sakib * tamim;
+// console.log(multiplication);
+
+var priceOfOrange =200;
+var priceOfOrange = priceOfOrange +50;
+priceOfOrange +=50;
+priceOfOrange -=50;
+priceOfOrange *=50;
+// console.log(priceOfOrange);
+
+var price1 = "50.52";
+var price2 = "55";
+//  console.log(price1 + price2);
+ 
+var numberprice1 = parseInt(price1)
+var numberprice2 = parseInt(price2)
+
+var numberPrice1 = parseFloat(price1)
+var numberPrice2 = parseInt(price2)
+
+//  console.log(numberPrice1 + numberPrice2);
+var totalNumberPrice = numberPrice1 + numberPrice2;
+// console.log(totalNumberPrice.toFixed(3));
+// console.log(typeof totalNumberPrice);
+ 
+var today = new Date();
+// console.log(today);
+// console.log(typeof today);
+
+// var num1 = 0.1;
+// var num2 = 0.3;
+// console.log(num1 + num2);
+
+var num1 = 0.1;
+var num2 = 0.2;
+// console.log(parseFloat((num1 + num2).toFixed(1)));
+
+
+// class -15 --Array.
+
+var myArray = ["little", "fish", "and", "many", "more",];
+// console.log(myArray);
+
+var oddNumber = [1,3,5,7,9,];
+// console.log(oddNumber);
+
+var vowels = ["a", "e", "i", "o", "u"];
+// console.log(vowels.length);
+// console.log(vowels.indexOf("o"));
+
+
+var myFevBooks = ["Ami", "Himu", "Opekkha", "Tomake", "Ei Ami"];
+// console.log(nameOfSomeBooks.indexOf("Himu"));
+myFevBooks[1] = "Mimu";
+// console.log(myFevBooks);
+indexOfMimu = myFevBooks;
+// console.log(indexOfMimu);
+myFevBook[indexOfMimu] ="Himu";
+ 
+console.log(myFevBook);
