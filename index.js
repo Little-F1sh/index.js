157dcfef78580ee9f32ef8ac60957490cebf2cdf
Little-F1sh <itsnihon1@gmail.com>
2023-06-22 05:47:08 +0600

    not done

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ var vowels = ["a", "e", "i", "o", "u"];
 // console.log(vowels.indexOf("o"));
 
 
-var myFevBooks = ["Ami", "Himu", "Opekkha", "Tomake", "Ei Ami"];
+{var myFevBooks = ["Ami", "Himu", "Opekkha", "Tomake", "Ei Ami"];
 // console.log(nameOfSomeBooks.indexOf("Himu"));
 myFevBooks[1] = "Mimu";
 // console.log(myFevBooks);
@@ -90,3 +90,118 @@ indexOfMimu = myFevBooks;
 myFevBook[indexOfMimu] ="Himu";
  
 console.log(myFevBook);
+// not done yat------------------------<
+}
+
+//--------play list -------
+    // -----Js Out put-----
+alert("I am shahariwar");
+document.write("I love my conuntry");
+console.log("I love my conuntry")
+
+    // -----single line comment----
+
+document.write("Name: Shahariwar <br/>");
+document.write("phone: 018-xxxxxxxx");
+    // -----multiline comment-----
+/*
+
+*/
+ document.write("Nihon <br>");
+ document.write("018xx-xxxxx <br>");
+document.write("Bangladesh <br>"); 
+
+    // -----variables-----
+
+var myName = "Shahariwar";
+ document.write(myName);
+ console.log(myName);
+
+var num = "20";
+var num2 = "20.36";
+var num3 = 20.36009786;
+var num4 = 20.36009786;
+num = toString(num); //---- for string.
+num = parseInt(num); //----for number.
+num = parseFloat(num2); //---- 1.1 nm=umber
+
+console.log(typeof(num)); //---for check type.
+
+console.log(num3.toFixed(2)); //---- for a fixd number display. (return = string).
+
+console.log(num4.toPrecision(4)); //---- for how much number i wont to show. (return = string).
+
+console.log(typeof(Number("12")));
+
+console.log(Number("     20.5"));
+
+console.log(Number(true));
+
+console.log(Number(false));
+
+    // -----string concatenation-----
+
+var firstName = "Shahariwar";
+var lastName = " Nihon";
+
+var fullName = firstName + lastName;
+
+    document.write("My Name Is " + fullName);
+
+var fullName = "Shahariwar Nihon";
+
+document.write(fullName + " Is My Name")
+
+var num1 = 30;
+var num2 = 40;
+
+document.write("Num1 =" + num1 +" and Num2 = " + num2);
+var text1 ="Bangladesh";
+var text= text1.slice(2,5);
+document.write(text);
+
+var text2 = " is a beautifull conuntry";
+var text = text1.concat(text2);
+
+document.write(text);
+
+
+
+    // -----string length-----
+
+var text ="bangladesh"
+
+var len = text.length;
+
+document.write("Number of charecter "+ text.length);
+
+var text =prompt("Enter Your Name ");//----how to take info from user.
+
+document.write("Number of charecter "+ text.length)
+
+var text = " Bangladesh";
+
+text.charAt(6)
+document.write(text.charAt(6));
+
+text = text.toUpperCase();
+text = text.toLocaleLowerCase();
+document.write(text);
+
+
+// ------test------
+{
+    // get first name from user
+    // get last name user
+    // add without using library function
+    // print full name
+    // convart full name into uppercase
+}
+
+var firstName = prompt ("Enter Your First Name");
+var lastName = prompt ("Enter Your last Name");
+
+var fullName = firstName +  lastName;
+fullName = fullName.toUpperCase();
+document.write(fullName + fullName.length);
+
